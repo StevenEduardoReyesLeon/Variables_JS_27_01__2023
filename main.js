d2b7e5a4fa345dbefd774c7d6c76352574c83745
StevenEduardoReyesLeon <stevenrl325@gmail.com>
2023-01-27 22:35:20 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,20 @@
+
+
+// String Cadenas de texto.
+// Number Valores numéricos.
+// Boolean Representa una entidad lógica y puede tener dos valores: true y false.
+// Object Un valor en memoria al que podemos acceder por un identificador.
+
+let apellido = "Reyes";
+let edad = 23;
+let estudia = true;
+let computador ={
+    marca: "Hp",
+    ram: "4 GB",
+    uso: "3 años",}
+
+    
+console.log(`Mi apellido es ${apellido}`);
+console.log(`Tengo ${edad} años`);
+console.log(`Estudio actualmente programacion: ${estudia}`);
+console.log(`Mi computador es ${computador.marca}, tiene ${computador.ram} de RAM, y lo tengo desde hace ${computador.uso}`);
